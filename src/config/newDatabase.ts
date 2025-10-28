fa2b5248b103be242a83cf4aab63e81830e563e1
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-10-29 00:54:04 +1100

    let's see

--- src/config/newDatabase.ts
+++ src/config/newDatabase.ts
@@ -20,8 +20,8 @@ export const newSupabase = createClient(newSupabaseUrl, newSupabaseAnonKey, {
 });
 
 // Legacy/Quick-book database client (old supabase)
-const oldSupabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const oldSupabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const oldSupabaseUrl = process.env.OLD_NEXT_PUBLIC_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL;
+const oldSupabaseAnonKey = process.env.OLD_NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const oldSupabase = (oldSupabaseUrl && oldSupabaseAnonKey)
   ? createClient(oldSupabaseUrl, oldSupabaseAnonKey, {
